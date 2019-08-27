366865e8d002f7d2a44de8be12128fcfda5955fd
Fabio Dos Santos <fabio2st@yahoo.com>
2019-08-27 13:41:44 -0300

    Proyectos, primer clase y primer test

--- EntidadesTest/Program.vb
+++ EntidadesTest/Program.vb
@@ -2,10 +2,15 @@ Imports System
 Imports Entidades
 Module Program
     Sub Main(args As String())
-        Dim cliente1 As New Cliente()
+        ' instanciación
+        Dim cliente1 As Cliente
+        'inicializacion
+        cliente1 = New Cliente()
+        'test de setters
         cliente1.Nombre = "Pepe"
         cliente1.Documento = 435345
         cliente1.FechaNacimiento = #01-01-2000#
+        'test de getters
         Console.WriteLine(cliente1.Nombre)
         Console.WriteLine(cliente1.Documento)
         Console.WriteLine(cliente1.FechaNacimiento)
